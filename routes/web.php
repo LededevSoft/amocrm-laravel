b456ea5affc3fc0d61ff2760bb83b6167bd5c9aa
Александр Лебедев <lebedev_as@bk.ru>
2023-10-23 13:21:46 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,1 @@
-<?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get("/amo/app", [\LebedevSoft\AmoCRM\Http\Controllers\AmoAppController::class, "amoApp"]);
-Route::get("/amo/auth", [\LebedevSoft\AmoCRM\Http\Controllers\AmoAppController::class, "amoAuth"]);
